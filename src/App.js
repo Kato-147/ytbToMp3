c283e9d5d1cb46c0d4120e329dcfa556e7242639
Tỏn nè <oroki147@gmail.com>
2024-09-05 22:18:31 +0700

    integarateAPI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { fetch } from "./services/ApiRequest";
 
 function App() {
   const [link, setLink] = useState("");
-  const [id, setId] = useState(null)
+  const [id, setId] = useState(null);
+  const [respone, setRespone] = useState(null);
+  const [disabled, setDisabled] = useState(false)
 
-  useEffect(()=>{
-    
-    id &&console.log(id);
-    
-  },[id])
+  useEffect(() => {
+    if (id) {
+      const fetchData = () => {
+        let interval = setInterval(async function () {
+          const res = await fetch(id);
+          if (res.status === "200" && res.data.status === "ok") {
+            setDisabled(false);
+            setRespone(res.data);
+            clearInterval(interval);
+          }
+        }, 1000);
+      };
+    }
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    if (respone) {
+      window.location.href = respone.link;
+    }
+  }, [respone]);
 
   function extractValueFromQuery(query) {
-    const parameter = 'v';
+    const parameter = "v";
     const urlParams = new URLSearchParams(query);
     const extractedId = urlParams.get(parameter);
 
     if (extractedId) {
-      setId(extractedId); // Update state with extracted ID
-      ; // Clear any previous error message
+      setId(extractedId); // Update state with extracted ID // Clear any previous error message
     } else {
-      console.log('Invalid YouTube link. Please enter a valid link with parameter "v".');
-       // Set error message for invalid links
+      console.log(
+        'Invalid YouTube link. Please enter a valid link with parameter "v".'
+      );
+      // Set error message for invalid links
     }
   }
 
@@ -37,16 +57,21 @@ function App() {
       {/* body */}
       <div id="body">
         {/* Input */}
-        <input 
-        placeholder="Youtube link here" 
-        value={link}
-        onChange={(e) => {setLink(e.target.value)}}
-        type="text" />
+        <input
+          placeholder="Youtube link here"
+          value={link}
+          onChange={(e) => {
+            setLink(e.target.value);
+          }}
+          type="text"
+        />
         <span>It might take a moment to convert to video</span>
       </div>
       <button
-      onClick={()=>extractValueFromQuery(link)}
-      >Download</button>
+       onClick={() => extractValueFromQuery(link)}
+       disabled={disabled}
+       className={disabled ? 'btn-disabled' : ''}
+       >Download</button>
     </div>
   );
 }
---
+++ src/services/ApiRequest.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const requestOptions = {
+  method: "GET",
+  url: "https://youtube-mp36.p.rapidapi.com/dl",
+  params: { id: "UxxajLWwzqY" },
+  headers: {
+    "x-rapidapi-key": "dc85c55751msh6997146eaf78fcap1ad9d7jsn5a0722ff02d9",
+    "x-rapidapi-host": "youtube-mp36.p.rapidapi.com",
+  },
+};
+
+const fetch = async (id) => {
+  requestOptions.params = { id };
+  const respone = await axios.request(requestOptions);
+  return respone;
+};
+
+export { fetch };
